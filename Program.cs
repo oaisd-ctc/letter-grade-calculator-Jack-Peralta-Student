5f0852d78c334602e2aaf9684f1680bdc83e7d6f
Jack-Peralta-Student <japecc2@student.oaisd.org>
2024-10-28 13:47:50 +0000

    Finish adding different grading scores from 0 through 100+.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        int score = int.Parse(Console.ReadLine());
+        if ( score >= 100)
+        {
+            Console.WriteLine("A+");
+            Console.WriteLine("Wow! 100…That's impressive!");
+        }
+        else if ( score >= 90)
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("Very nice!");
+        }
+        else if ( score >= 80)
+        {
+            Console.WriteLine("B");
+            Console.WriteLine("Keep it up!");
+        }
+        else if ( score >= 70)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("You got this, keep working!");
+        }
+        else if ( score >= 60)
+        {
+            Console.WriteLine("D");
+            Console.WriteLine("Keep Trying!"); 
+        }
+        else
+        {
+            Console.WriteLine("E");
+            Console.WriteLine("Don't give up!");
+
+        }
+    }
+}
